92f2979f658b8d3190a22fe55d46e8d777d96f92
Your Name <vverma4313@gmail.com>
2020-11-15 00:29:27 +0530

    Added navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <nav className="navigation">
-                        <div className="username" >
-                            <p>User</p>
-                        </div>
-                        <div>
-                            <input type="search" className="search-box" placeholder="search here..." />
-                            <input type="search" className="search-box" placeholder="search here..." />
-                        </div>
-                        <div className="logout">
-                            <p>Logout</p>
-                        </div>
-                </nav>
+  logout = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <nav className="navigation">
+          <div className="username">User</div>
+          <div className="search-box">
+            <input type="search" target="search1"  placeholder="search here..." />
+          </div>
+          <div className="search-box">
+            <input type="search" placeholder="search here..." />
+          </div>
+          <Link to="/">
+            <div className="logout" onClick={this.logout}>
+              Logout
             </div>
-        )
-    }
+          </Link>
+        </nav>
+        <div className="frame-container">
+            <iframe title="search1" className="frame-1" ></iframe>
+            <iframe title="search2" className="frame-2" ></iframe>
+        </div>
+      </div>
+    );
+  }
 }
